425b19d178a56b8cd7e99b619bffc2445341d879
Christopher Kim <chrisms91@gmail.com>
2020-07-22 01:29:35 -0700

    add pug and initial view templates with header and footer

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import { localsMiddleware } from './middlewares';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globalRouter';
@@ -11,11 +12,13 @@ import routes from './routes';
 const app = express();
 
 // middlewares
+app.use(helmet()); //security stuff
+app.set('view engine', 'pug');
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet()); //security stuff
 app.use(morgan('dev')); // logger
+app.use(localsMiddleware);
 
 // routes
 app.use(routes.home, globalRouter);
