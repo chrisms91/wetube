c1cf0e0e52bf46dba7c850f7c24376dea3b663e6
Christopher Kim <chrisms91@gmail.com>
2020-07-24 00:54:59 -0700

    add video and comment schema and model, add basic video uploading functionality

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 // middlewares
 app.use(helmet()); //security stuff
 app.set('view engine', 'pug');
+app.use('/uploads', express.static('uploads'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
