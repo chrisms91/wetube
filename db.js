c1ce134f0bcfa0c66876af26794c65792451f804
Christopher Kim <chrisms91@gmail.com>
2020-07-23 22:19:26 -0700

    add basic flow of controller

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 33244,
+    title: 'Video awesome',
+    description: 'This is something I love',
+    views: 24,
+    videoFile:
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Nicolad',
+      email: 'nico@las.com',
+    },
+  },
+  {
+    id: 11297,
+    title: 'Super',
+    description: 'Tthishis description',
+    views: 24,
+    videoFile:
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Nicolad',
+      email: 'nico@las.com',
+    },
+  },
+  {
+    id: 92394,
+    title: 'Perfect',
+    description: 'perfectooooo',
+    views: 24,
+    videoFile:
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Nicolad',
+      email: 'nico@las.com',
+    },
+  },
+  {
+    id: 998877,
+    title: 'Sexy',
+    description: 'Tohhhhhh',
+    views: 24,
+    videoFile:
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Nicolad',
+      email: 'nico@las.com',
+    },
+  },
+];
