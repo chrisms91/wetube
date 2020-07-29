e15e760537dc073b1dd995e7c7b175d0d99efb66
Christopher Kim <chrisms91@gmail.com>
2020-07-28 22:13:54 -0700

    add edit user profile

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -123,7 +123,55 @@ export const userDetail = async (req, res) => {
   }
 };
 
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) => {
   res.render('editProfile', { pageTitle: 'Edit Profile' });
-export const changePassword = (req, res) =>
+};
+
+export const postEditProfile = async (req, res) => {
+  const {
+    body: { name, email },
+    file,
+  } = req;
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      name,
+      email,
+      avatarUrl: file ? file.path : req.user.avatarUrl,
+    });
+    res.redirect(routes.me);
+  } catch (error) {
+    res.redirect(routes.editProfile);
+  }
+};
+
+export const getChangePassword = (req, res) =>
   res.render('changePassword', { pageTitle: 'Change Password' });
+
+export const postChangePassword = async (req, res) => {
+  const {
+    body: { oldPassword, newPassword, newPassword1 },
+  } = req;
+  try {
+    if (newPassword !== newPassword1) {
+      res.status(400);
+      res.redirect(`/users${routes.changePassword}`);
+      return;
+    }
+    console.log('changing password..');
+    await req.user.changePassword(oldPassword, newPassword).then(
+      (result) => {
+        console.log('changed password');
+        console.log(result);
+        res.redirect(routes.me);
+      },
+      (error) => {
+        console.log('something wrong');
+        console.log(error);
+        res.redirect(`/users${routes.changePassword}`);
+      }
+    );
+    // res.redirect(routes.me);
+  } catch (error) {
+    res.redirect(`/users${routes.changePassword}`);
+  }
+};
